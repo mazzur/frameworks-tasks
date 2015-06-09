0a85a268926e1a32b4020548f6efb73644858cb3
mazzur <mazzur55@gmail.com>
2015-06-10 01:48:58 +0300

    [angular] - add cart -v1

--- angular-phonecat/app/js/controllers.js
+++ angular-phonecat/app/js/controllers.js
@@ -4,19 +4,45 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$rootScope', 'Phone',
+  function($scope, $rootScope, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
+
+    $scope.addToCart = function(phone) {
+      $rootScope.$broadcast('add-to-cart', phone);
+    }
   }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$rootScope', '$routeParams', 'Phone',
+  function($scope, $rootScope, $routeParams, Phone) {
     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
       $scope.mainImageUrl = phone.images[0];
     });
 
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
+    };
+
+    $scope.addToCart = function(phone) {
+      $rootScope.$broadcast('add-to-cart', {
+        id: phone.id,
+        name: phone.name,
+        imageUrl: phone.images[0]
+      });
     }
+  }]);
+
+phonecatControllers.controller('CartCtrl', ['$scope',
+  function($scope) {
+    $scope.phones = [];
+
+    $scope.$on('add-to-cart', function(event, phone) {
+      if (!$scope.phones.some(function(one) {
+            return one.id === phone.id;
+          })) {
+        $scope.phones.push(phone);
+        $scope.number = $scope.phones.length;
+      }
+    });
   }]);
